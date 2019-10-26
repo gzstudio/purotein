68e3601459fdfa842911880d13609a1547c945a4
YR <pitt.rasananda@gmail.com>
2019-10-26 14:46:18 +1100

    add partial structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX  }, uniqueness: true
     has_secure_password
     validates :password, presence: true, length: {minimum: 6 }
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
 end
